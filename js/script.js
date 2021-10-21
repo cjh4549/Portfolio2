3e9923801e6e9f7e6fa8ed84e5a3d40be9fbddf2
Jun Choi <cjh4549@gmail.com>
2021-10-20 23:12:40 -0400

    added gsap animations to header elements

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
-gsap.from(".hero__left__bg", {duration: 2, width: 0,  ease: Power2.easeOut});
-gsap.to(".hero__left__text", {duration: 1, delay: 1.4, x: 150});
+const heroLeftBg = document.querySelector('.hero__left__bg');
+
+const tl = new TimelineMax();
+
+tl.from(".hero__left__bg", {duration: 1.5, width: 0,  ease: Power2.easeOut});
+tl.from(".hero__left__text", {duration: 1, opacity: 0, x: 150});
+tl.from (".hero__right", {duration: 1, opacity: 0, y: 100})
