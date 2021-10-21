8e355540cb4930d42f81481162a5c456591fb800
Jun Choi <cjh4549@gmail.com>
2021-10-20 22:12:52 -0400

    added new container for header text for gsap animation

--- js/script.js
+++ js/script.js
@@ -1,12 +1,2 @@
-const hero = document.querySelector('.hero');
-// const links = document.querySelectorAll('a');
-
-hero.addEventListener('click', function(e){
-    console.log(e.target);
-})
-
-document.querySelectorAll("*").forEach(i => i.addEventListener(
-    "click",
-    e => {
-        console.log(e.target);
-    }));
+gsap.from(".hero__left__bg", {duration: 2, width: 0,  ease: Power2.easeOut});
+gsap.to(".hero__left__text", {duration: 1, delay: 1.4, x: 150});
