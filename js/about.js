8c0936bac01de48933d94198d48cee9ed640b23d
Jun Choi <cjh4549@gmail.com>
2021-10-24 15:58:56 -0400

    removed arrow in about section

--- js/about.js
+++ js/about.js
@@ -1,16 +1,14 @@
 const heroDescription = document.querySelector('.hero__description');
 const h2 = document.querySelector('.hero__about h2');
-const arrow = document.querySelector('.hero__description__arrow');
 const pia = document.querySelector('.hero__description--pia')
 const development = document.querySelector('.hero__development');
 
 const tl = new TimelineMax();
 
 
-tl.from(h2, {duration: 0.8, opacity: 0, y: -50});
-tl.from(heroDescription, {duration: 1, opacity: 0, x: 150});
-tl.from(development, {duration: 1, opacity: 0, x: 100});
-tl.from(arrow, {duration: 1, opacity: 0});
-tl.from(pia, {duration: 2, opacity: 0, ease: Power2.easeOut});
+tl.from(h2, {duration: 0.5, opacity: 0, y: -30});
+tl.from(heroDescription, {duration: 0.7, opacity: 0, x: 70});
+tl.from(development, {duration: 0.7, opacity: 0, x: 50});
+tl.from(pia, {duration: 1.6, opacity: 0, ease: Power2.easeOut});
 
     
--- js/project.js
+++ js/project.js
@@ -14,7 +14,7 @@ const vh = (height) => window.innerHeight * (height/100)
 
 const tl = new TimelineMax();
 
-tl.to(heroLeftBg, {duration: 1.2, delay: 0.3, x: -200, height: vh(170), width: vw(180), ease: Power2.easeInOut});
+tl.to(heroLeftBg, {duration: 0.8, delay: 0.2, x: -200, height: vh(170), width: vw(180), ease: Power2.easeInOut});
 tl.to(project, {opacity: 1, duration: -2});
 tl.to(projectOverlay, {height: 0, duration: 0.8, ease: Power2.easeOut});
 tl.from(projectText, {opacity: 0, duration: 1, y: 50, ease: Power2.easeInOut});
@@ -55,9 +55,6 @@ function delay (url) {
     
     projectTextContainer.style.opacity = '0'
     projectTextContainer.style.transition = 'all 0.7s';
-    
-    // projectOverlay.style.height = '100%'
-    // projectOverlay.style.transition = 'all 0.5s';
 
     setTimeout(() => {
         window.location = url
