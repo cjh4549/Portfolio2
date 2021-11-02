1ca3a628acf763fec2df14f88b5f4e625e3bd0c6
Jun Choi <cjh4549@gmail.com>
2021-11-02 13:39:53 -0400

    added hamburger x animation

--- js/about.js
+++ js/about.js
@@ -3,9 +3,20 @@ const h2 = document.querySelector('.hero__about h2');
 const pia = document.querySelector('.hero__description--pia')
 const development = document.querySelector('.hero__development');
 
+const hamburger = document.querySelector('.hamburger');
+
 const tl = new TimelineMax();
 
 tl.from(h2, {duration: 0.5, opacity: 0, y: -30});
 tl.from(heroDescription, {duration: 0.7, opacity: 0, x: 70});
 tl.from(development, {duration: 0.7, opacity: 0, x: 50});
 tl.from(pia, {duration: 1.6, opacity: 0, ease: Power2.easeOut});
+
+hamburger.addEventListener('click', function(){
+    if (!hamburger.classList.contains('active')) {
+        hamburger.classList.add('active');
+    } else {
+        hamburger.classList.remove('active');
+    }
+    
+})
