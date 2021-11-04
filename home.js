5993111f1b0d62cb46de266bda5800343543035b
Jun Choi <cjh4549@gmail.com>
2021-11-04 15:30:31 -0400

    added closing functionality for hamburger menu

--- home.js
+++ home.js
@@ -5,11 +5,16 @@ const hamburgerMenu = document.querySelector('.fa-bars');
 const hamburgerX = document.querySelector('.fa-times');
 
 hamburger.addEventListener('click', function(){
-    if (!hamburger.classList.contains('hamburgerOpen')) {
+    if (!navbarItems.classList.contains('hamburgerOpen')) {
         navbarItems.classList.add('hamburgerOpen')
         hamburgerMenu.style.opacity = '0';
-        hamburgerMenu.style.transition = 'all 0.5s';
+        hamburgerMenu.style.transition = 'all 0.3s';
         hamburgerX.style.opacity = '1';
-        hamburgerX.style.transition = 'all 0.5s';
-    } 
+        hamburgerX.style.transition = 'all 0.3s';
+    } else if (navbarItems.classList.contains('hamburgerOpen')) {
+        navbarItems.classList.remove('hamburgerOpen');
+        hamburgerMenu.style.opacity = '1';
+        hamburgerX.style.opacity = '0';
+        hamburgerX.style.transition = 'all 0s';
+    }
 })
