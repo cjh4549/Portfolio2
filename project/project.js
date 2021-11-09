2760e4933d711a681f7a006f6bcd6d5a4a0ccb10
Jun Choi <cjh4549@gmail.com>
2021-11-09 17:46:41 -0500

    added contact page

--- project/project.js
+++ project/project.js
@@ -13,10 +13,21 @@ new Glider(document.querySelector('.project__glider'), {
           breakpoint: 1200,
           settings: {
             // Set to `auto` and provide item width to adjust to viewport
-            slidesToShow: 'auto',
+            slidesToShow: 1,
             slidesToScroll: 'auto',
-            itemWidth: 700,
+            itemWidth: 100,
             duration: 0.25
           }
-    }]
+    },
+    {
+        // screens greater than >= 1024px
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          itemWidth: 150,
+          duration: 0.25
+        }
+      }
+]
 })
