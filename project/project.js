1ed61fca4709ed96d26c52d1a7729a0548aed3d3
Jun Choi <cjh4549@gmail.com>
2021-11-09 11:40:44 -0500

    working on glider responsiveness

--- project/project.js
+++ project/project.js
@@ -1,9 +1,22 @@
 new Glider(document.querySelector('.project__glider'), {
     slidesToShow: 1,
-    dots: '#dots',
+    dots: '#resp-dots',
     draggable: true,
+    scrollLock: true,
     arrows: {
         prev: '.glider-prev',
         next: '.glider-next'
-    }
+    },
+    responsive: [
+        {
+          // screens greater than >= value
+          breakpoint: 1200,
+          settings: {
+            // Set to `auto` and provide item width to adjust to viewport
+            slidesToShow: 'auto',
+            slidesToScroll: 'auto',
+            itemWidth: 700,
+            duration: 0.25
+          }
+    }]
 })
