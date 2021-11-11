af411de5195029f2777fef6b6e6884e61bebbc0a
Jun Choi <cjh4549@gmail.com>
2021-11-11 14:09:40 -0500

    added responsiveness to project page for tablet and bigger phones

--- project/project.js
+++ project/project.js
@@ -1,32 +1,8 @@
-new Glider(document.querySelector('.project__glider'), {
-    slidesToShow: 1,
-    dots: '#resp-dots',
-    draggable: true,
-    scrollLock: true,
-    arrows: {
-        prev: '.glider-prev',
-        next: '.glider-next'
-    },
-    responsive: [
-        {
-          // screens greater than >= 1200px
-          breakpoint: 1200,
-          settings: {
-            // Set to `auto` and provide item width to adjust to viewport
-            slidesToShow: 1,
-            slidesToScroll: 'auto',
-            itemWidth: 3000,
-            duration: 0.25
-          }
-    },
-    {
-        // screens greater than >= 1024px
-        breakpoint: 1024,
-        settings: {
-          slidesToShow:  'auto',
-          slidesToScroll: 'auto',
-          duration: 0.25
-        }
-      }
-]
+const glide = new Glide('#options-type', {
+  type: select.value,
+  focusAt: 'center',
+  perView: 1
 })
+
+
+glide.mount()
